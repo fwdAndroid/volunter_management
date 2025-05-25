4fdd339328028ae9ec48316978b19d5dc5d4d015
fwdAndroid <fwdkaleem@gmail.com>
2025-05-25 14:01:51 +0500

    Splash Screen Done

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:volunter_management/screens/auth/login_screen.dart';
+import 'package:volunter_management/uitls/colors.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _checkAuth();
+  }
+
+  void _checkAuth() async {
+    await Future.delayed(const Duration(seconds: 3)); // Simulate splash time
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => LoginScreen()),
+    );
+    // User? user = FirebaseAuth.instance.currentUser;
+
+    // if (user != null) {
+    //   // User is signed in, navigate to Home Page
+    //   Navigator.pushReplacement(
+    //     context,
+    //     MaterialPageRoute(builder: (context) => MainDashboard()),
+    //   );
+    // } else {
+    //   // No user signed in, navigate to Login Page
+    //   Navigator.pushReplacement(
+    //     context,
+    //     MaterialPageRoute(builder: (context) => LoginScreen()),
+    //   );
+    // }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Center(child: Image.asset('assets/logo.png'))],
+      ),
+    );
+  }
+}
---
+++ lib/uitls/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+final backgroundColor = Color(0xffF5F5F5);
