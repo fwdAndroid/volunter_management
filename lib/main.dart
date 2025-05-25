6b6f74ceb183491693e7f52a526da3363f365d41
fwdAndroid <fwdkaleem@gmail.com>
2025-05-25 14:32:54 +0500

    Firebase Initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:volunter_management/firebase_options.dart';
 import 'package:volunter_management/screens/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
